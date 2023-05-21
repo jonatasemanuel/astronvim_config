68db277b4909c636e1cb8a7bd2d67a18e55985e0
Jonatas Silva <105525509+jonatasemanuel@users.noreply.github.com>
2023-05-21 16:50:14 -0300

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -23,12 +23,5 @@ return {
     },
   },
   
-  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
-  {
-    "m4xshen/smartcolumn.nvim",
-    opts = {
-      colorcolumn = 120,
-      disabled_filetypes = { "help" },
-    },
-  },
+  
 }
