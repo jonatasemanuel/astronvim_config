86cb7306822087e9e05c05f6d240de11e437c5f0
Jonatas Silva <105525509+jonatasemanuel@users.noreply.github.com>
2023-05-21 16:54:35 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -22,8 +22,7 @@ return {
    
   -- Set colorscheme to use
   colorscheme = "astrodark", 
-  
-  set t_Co=256,
+ 
 
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
