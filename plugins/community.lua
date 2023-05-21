35e8e6759e39a0d678096e38e35a3be20a9a07b7
Jonatas Silva <105525509+jonatasemanuel@users.noreply.github.com>
2023-05-21 16:16:17 -0300

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -23,21 +23,21 @@ return {
     },
   },
   -- { import = "astrocommunity.completion.copilot-lua" },
-  { -- further customize the options set by the community
-    "copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
-        },
-      },
-    },
-  },
+  -- { -- further customize the options set by the community
+  --  "copilot.lua",
+  --  opts = {
+  --    suggestion = {
+  --      keymap = {
+  --        accept = "<C-l>",
+  --       accept_word = false,
+  --      accept_line = false,
+  --     next = "<C-.>",
+  --    prev = "<C-,>",
+  --   dismiss = "<C/>",
+  -- },
+  --},
+  -- },
+  --},
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
   {
     "m4xshen/smartcolumn.nvim",
