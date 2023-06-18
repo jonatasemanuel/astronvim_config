f1ac5fe514aa79f3158c561e8d0492884cb81302
Jonatas Silva <105525509+jonatasemanuel@users.noreply.github.com>
2023-06-17 23:55:48 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ return {
   },
    
   -- Set colorscheme to use
-  colorscheme = "kanagawa", 
+  -- colorscheme = "kanagawa", 
  
 
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
